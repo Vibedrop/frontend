c81ee9a37ad4a0c4a976ad84a4ab9024525be9e3
unknown <kristoffer.larsson.02@gmail.com>
2025-05-26 13:45:52 +0200

    ft-change-password

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -2,24 +2,32 @@
 import React, { useState, ChangeEvent, KeyboardEvent } from "react";
 import { BACKEND_URL } from "@/utilities/config";
 import { useAuthStore } from "@/stores/useAuthStore";
+import { Text } from "@radix-ui/themes";
 
 export default function ProfilePage() {
-      const { checkAuth } = useAuthStore();
+    const { checkAuth } = useAuthStore();
     const user = useAuthStore(state => state.user);
-    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [isEditingName, setIsNameEditing] = useState<boolean>(false);
+    const [isEditingPassword, setIsPasswordEditing] = useState<boolean>(false);
     const [name, setName] = useState<string>(user?.username || "");
+    const [password, setPassword] = useState<string>(user?.username || "");
 
-    const handleNameClick = () => setIsEditing(true);
+    const handleNameClick = () => setIsNameEditing(true);
+    const handlePasswordClick = () => setIsPasswordEditing(true);
 
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleNameInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         setName(e.target.value);
     };
+    const handlePasswordInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
 
-    const handleBlur = () => setIsEditing(false);
+    const handleNameBlur = () => setIsNameEditing(false);
+    const handlePasswordBlur = () => setIsPasswordEditing(false);
 
     async function changeName() {
         try {
-            const response = await fetch(`${BACKEND_URL}/users/${user?.id}`, {
+            const response = await fetch(`${BACKEND_URL}/users/${user?.id}/username`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -40,30 +48,78 @@ export default function ProfilePage() {
             console.error(error);
         }
     }
+    async function changePassword() {
+        try {
+            const response = await fetch(`${BACKEND_URL}/users/${user?.id}/password`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+                body: JSON.stringify({
+                    password: password,
+                }),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log("changePassword", data);
+                await checkAuth();
+            } else {
+                throw new Error("error");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    const handleNameKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter") {
-            setIsEditing(false);
+            setIsNameEditing(false);
             changeName();
+        }
+    };
+        const handlePasswordKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            setIsPasswordEditing(false);
+            changePassword();
         }
     };
 
     return (
+        <>
         <div>
-            {isEditing ? (
+        <Text>Name:</Text>
+            {isEditingName ? (
                 <input
-                    type="text"
-                    value={name}
-                    autoFocus
-                    onChange={handleInputChange}
-                    onBlur={handleBlur}
-                    onKeyDown={handleKeyDown}
+                type="text"
+                value={name}
+                autoFocus
+                onChange={handleNameInputChange}
+                onBlur={handleNameBlur}
+                onKeyDown={handleNameKeyDown}
                 />
             ) : (
                 <span onClick={handleNameClick} style={{ cursor: "pointer" }}>
                     {name}
                 </span>
             )}
+            </div>
+            <div>
+                <Text>Pasword:</Text>
+            {isEditingPassword ? (
+                <input
+                type="text"
+                autoFocus
+                onChange={handlePasswordInputChange}
+                onBlur={handlePasswordBlur}
+                onKeyDown={handlePasswordKeyDown}
+                />
+            ) : (
+                <span onClick={handlePasswordClick} style={{ cursor: "pointer" }}>
+                    Change
+                </span>
+            )}
         </div>
+            </>
     );
 }
